2a5ef131f985e4e51ff18fda0374e9ff8d2314bc
Andrew McCall <amccall@ciginsurance.com>
2018-02-06 13:38:36 -0800

    Added logging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #A very simple bash script
 
+echo "Running Deployment Script"
+echo "Creating Deployment folder"
 mkdir deployment
+echo "Deployment folder created. Touching file"
 touch deployment/the_file.txt
+echo "File touched. Writing contents"
 echo "This is the big deployment here!" > deployment/the_file.txt
