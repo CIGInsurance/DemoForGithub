f8e2248592a78e65d94b2bd5183bfe82b32ad5f4
Andrew McCall <amccall@ciginsurance.com>
2018-02-06 13:52:56 -0800

    Simpler even still.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#A very simple bash script
-
-echo "Running Deployment Script"
-echo "Creating Deployment folder"
-mkdir deployment
-echo "Deployment folder created. Writing contents"
-cp ./the_file.txt ./deployment/the_file.txt
