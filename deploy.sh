5d6024671ee6ceee968605c20f0975a1a3d565cc
Andrew McCall <amccall@ciginsurance.com>
2018-02-06 13:51:40 -0800

    Simplified deployment script to only copy file because jenkins.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@
 echo "Running Deployment Script"
 echo "Creating Deployment folder"
 mkdir deployment
-echo "Deployment folder created. Touching file"
-touch deployment/the_file.txt
-echo "File touched. Writing contents"
-echo "This is the big deployment here!" >> deployment/the_file.txt
+echo "Deployment folder created. Writing contents"
+cp ./the_file.txt ./deployment/the_file.txt
